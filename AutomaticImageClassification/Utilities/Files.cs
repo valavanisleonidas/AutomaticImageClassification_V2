a82b2c8adba038a06ed99b85ff2ccc0177b4964d
Leonidas Valavanis <l.valavanis@dev-gr.clmsuk.com>
2016-08-12 17:02:27 +0300

    Finished Boc Library
    Former-commit-id: b5af702121c780d593788b1b840db4e516642302
    Former-commit-id: ef5ba78cf9ae70c4711a9dad9dd512ebd74b9e5b

---
+++ AutomaticImageClassification/Utilities/Files.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticImageClassification.Utilities
+{
+    public class Files
+    {
+        public static string[] GetFilesFrom(string searchFolder, string[] filters = null, bool isRecursive = true)
+        {
+            if (filters == null)
+                filters = new[] { "jpg", "jpeg", "png", "gif", "tiff", "bmp" };
+
+            List<string> filesFound = new List<string>();
+            var searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            foreach (var filter in filters)
+            {
+                filesFound.AddRange(Directory.GetFiles(searchFolder, string.Format("*.{0}", filter), searchOption));
+            }
+            return filesFound.ToArray();
+        }
+
+        public static string[] GetFilesFrom(string searchFolder,int filesFromEachSubFolder, string[] filters = null, bool isRecursive = true)
+        {
+            if (filters == null)
+                filters = new[] { "jpg", "jpeg", "png", "gif", "tiff", "bmp" };
+
+            string[] filesInDirectory = Directory.GetDirectories(searchFolder);
+            List<string> filesFound = new List<string>();
+            var searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            foreach (string subfolder in filesInDirectory)
+            {
+                filesFound.AddRange(Directory.GetFiles(subfolder, string.Format("*.{0}", filters), searchOption).Take(filesFromEachSubFolder));
+            }
+            return filesFound.ToArray();
+        }
+
+        public static void WriteFile(string fileToWrite,List<double[]> contentList )
+        {
+            string content = "";
+            foreach (double[] feature in contentList)
+            {
+                content += string.Join(" ", feature);
+                content += "\r\n";
+            }
+            // Write the string to a file.
+            File.WriteAllText(fileToWrite, content);
+        }
+
+        public static List<double[]> ReadFileToList(string path)
+        {
+            return ReadFileToArray(path).ToList();
+        }
+
+        public static double[][] ReadFileToArray(string path)
+        {
+            string[] content = File.ReadAllLines(path);
+           
+            
+
+            var result = File.ReadAllLines(path)
+                .Select(l => l.Split(' ').Select(i => double.Parse(i)).ToArray())
+                .ToArray();
+            return result;
+        }
+    }
+}
--- AutomaticImageClassificationTests/UnitTest1.cs
+++ AutomaticImageClassificationTests/UnitTest1.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using boclibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AutomaticImageClassification.Utilities;
+using java.awt.image;
 using net.sf.javaml.core.kdtree;
 
 
@@ -12,41 +14,97 @@ namespace AutomaticImageClassificationTests
     [TestClass]
     public class UnitTest1
     {
+        //TODO THEMA OTAN GRAFW DECIMAL KAI DIAVAZW ME TELEIES KAI KOMMATA
+        //TODO cannot read Dictionary
         [TestMethod]
-        public void CanCreatePalette()
+        public void CanCreateLBOC()
         {
-            string file = "as.txt";
-            var sampleImgs = new string[]{ @"C:\Users\l.valavanis\Desktop\Leo Files\images\Photos\1.JPG" };
-            int[][] palete = BoCLibrary.createPalette(file,sampleImgs,64,10,10,true,ColorConversion.ColorSpace.RGB);
+            int noOfVWords = 10;
+            int NumOfcolors = 10;
+            int resize = 16;
+            int patches = 16;
+            ColorConversion.ColorSpace colorspace = ColorConversion.ColorSpace.RGB;
+            string paleteFile = @"C:\Users\l.valavanis\Desktop\palete.txt";
+            string dictionaryFile = @"C:\Users\l.valavanis\Desktop\dictionary.txt";
+            string trainFile= @"C:\Users\l.valavanis\Desktop\train.txt";
+            string testFile = @"C:\Users\l.valavanis\Desktop\test.txt";
 
-            Console.WriteLine("palete");
-            foreach (int[] i in palete)
-            {
-                Console.WriteLine(i[0] + " " + i[1] + " " + i[2]);
-            }
+            string baseFolder = @"C:\Users\l.valavanis\Desktop\Leo Files\DBs\Clef2013\Compound";
+            string trainPath = Path.Combine(baseFolder, "Train");
+            string testPath = Path.Combine(baseFolder, "Test");
 
+            //Create Palette
+            var sampleImgs = Files.GetFilesFrom(trainPath, 2);
+            int[][] palete = BoCLibrary.createPalette(paleteFile, sampleImgs, resize, patches, NumOfcolors, true, colorspace);
             List<double[]> colors = AutomaticImageClassification.Utilities.Arrays.ConvertIntArrayToDoubleList(palete);
-            
-            Console.WriteLine("lsit");
-            Console.WriteLine("--------------------");
-            foreach (double[] color in colors)
-            {
-                Console.WriteLine(color[0] + " " + color[1] + " " + color[2]);
-            }
-            
+            Files.WriteFile(paleteFile, colors);
+
+            //Create Dictionary
             KDTree tree = AutomaticImageClassification.Utilities.KDTreeImplementation.createTree(colors);
+            Clusterer dictionary = BoCLibrary.createDictionary(dictionaryFile, sampleImgs, resize, patches, noOfVWords, palete,
+                colorspace, tree);
+        
 
-            int index =AutomaticImageClassification.Utilities.KDTreeImplementation.SearchTree(new double[] {100, 100 ,100}, tree);
-            Console.WriteLine("closest to 100 100 100");
-            Console.WriteLine(palete[index][0] + " " + palete[index][1] + " " + palete[index][2] + " " );
+            //Feature extraction LBOC
+            List<double[]> trainFeatures = new List<double[]>();
+            foreach (string train in Files.GetFilesFrom(trainPath))
+            {
+                BufferedImage image = IO.getImage(train);
+                double[] vec = BoCLibrary.getLBoC(image, palete, dictionary, resize, patches, colorspace, tree);
+                trainFeatures.Add(vec);
+            }
+            Files.WriteFile(trainFile, trainFeatures);
 
+            List<double[]> testFeatures = new List<double[]>();
+            foreach (string test in Files.GetFilesFrom(testPath))
+            {
+                BufferedImage image = IO.getImage(test);
+                double[] vec = BoCLibrary.getLBoC(image, palete, dictionary, resize, patches, colorspace, tree);
+                testFeatures.Add(vec);
+            }
+            Files.WriteFile(testFile, testFeatures);
 
-            Clusterer dictionary = BoCLibrary.createDictionary(file, sampleImgs, 64, 10, 10, palete,
-                ColorConversion.ColorSpace.RGB, tree);
-
-
-            
 
         }
+
+        [TestMethod]
+        public void CanReadAllImagesFromFolder()
+        {
+            string searchFolder = @"C:\Users\l.valavanis\Desktop\Leo Files\DBs\clef2016\subfigure\SubfigureClassificationTraining2016_Enriched";
+           
+            string[] files = Files.GetFilesFrom(searchFolder);
+            
+            Console.WriteLine(files.Length);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            
+        }
+
+        [TestMethod]
+        public void CanWriteFileToFolder()
+        {
+            List<double[]> features = new List<double[]>();
+            double[] lines = new [] {012 , 1.1 ,2.2};
+            double[] lines2 = new[] { 0.0, 5.1, 2.1 };
+            features.Add(lines);
+            features.Add(lines2);
+
+            string fileToWrite = @"C:\Users\l.valavanis\Desktop\test.txt";
+            Files.WriteFile(fileToWrite, features);
+            
+        }
+
+        [TestMethod]
+        public void CanReadFile()
+        {
+            string path = @"C:\Users\l.valavanis\Desktop\dictionary.txt";
+            var result = Files.ReadFileToArray(path);
+            string fileToWrite = @"C:\Users\l.valavanis\Desktop\testpalete.txt";
+            Files.WriteFile(fileToWrite, result.ToList());
+
+        }
+
     }
 }

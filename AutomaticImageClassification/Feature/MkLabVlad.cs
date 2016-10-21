ee39738b2603a663298f1d95de96a9c404696f18
Valavanis Leonidas <valavanisleonidas@gmail.com>
2016-10-21 15:11:02 +0300

    -add MKLab vlad algorithm -equality comparison rename
    Former-commit-id: c825327cb6a4acc8878193f177bf8d4d51ef24cc [formerly 01c1bdb31219ab1392d7a17b576ba318829b4850]
    Former-commit-id: 75717988c9b015e8ceebd36ca544d576067ce926
    Former-commit-id: 61e67ae68e7f85ff2f942bf30fe17d6d290fcf59 [formerly 82054c1b39be4dc3cc799b262d20e48e9fbefcdb]
    Former-commit-id: 9a39597667a607408178aee0ea7aea1ba1911b1f
    Former-commit-id: 517a684a112466335a75bbd1894c835198c86be3 [formerly 7ad844042bb917975b5cd81ff4909006f47924c8]
    Former-commit-id: e18d58bb8c08482b2e12f8105b7f10cbddb71676

---
+++ AutomaticImageClassification/Feature/MkLabVlad.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using gr.iti.mklab.visual.aggregation;
+
+namespace AutomaticImageClassification.Feature
+{
+    public class MkLabVlad : IFeatures
+    {
+        private IFeatures _featureExtractor;
+        private VladAggregator _vlad;
+
+        public MkLabVlad()
+        {
+            _featureExtractor = new MkLabSurf();
+        }
+
+        public MkLabVlad(IFeatures extractor)
+        {
+            _featureExtractor = extractor;
+        }
+
+        public MkLabVlad(List<double[]> codebook)
+        {
+            _featureExtractor = new MkLabSurf();
+            _vlad = new VladAggregator(codebook.ToArray());
+        }
+
+        public MkLabVlad(List<double[]> codebook, IFeatures extractor)
+        {
+            _featureExtractor = extractor;
+            _vlad = new VladAggregator(codebook.ToArray());
+        }
+
+        public double[] ExtractHistogram(string input)
+        {
+            return _vlad.aggregate(_featureExtractor.ExtractDescriptors(input).ToArray());
+        }
+
+        public List<double[]> ExtractDescriptors(string input)
+        {
+            return _featureExtractor.ExtractDescriptors(input);
+        }
+
+        public override string ToString()
+        {
+            return "Vlad_" + _featureExtractor;
+        }
+
+    }
+}
---
+++ AutomaticImageClassification/Utilities/CustomGenericArrayEqualityComparator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticImageClassification.Utilities
+{
+
+    public class CustomGenericArrayEqualityComparator<T> : IEqualityComparer<T[]>
+    {
+        public bool Equals(T[] x, T[] y)
+        {
+            if (ReferenceEquals(x, y))
+                return true;
+
+            if (x == null || y == null)
+                return false;
+
+            return x.SequenceEqual(y);
+        }
+
+        public int GetHashCode(T[] obj)
+        {
+            if (obj == null)
+                return 0;
+
+            var hash = 17;
+
+            unchecked
+            {
+                foreach (T s in obj)
+                    hash = hash * 23 + ((s == null) ? 0 : s.GetHashCode());
+            }
+
+            return hash;
+        }
+    }
+}

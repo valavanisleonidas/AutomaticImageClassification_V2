8625825d200f8d5852f90728f05d506267ef74c8
DEV-GR\l.valavanis <valavanisleonidas@gmail.com>
2016-11-10 13:24:43 +0200

    -removed Windows Forms application and converted to Library -added MVC Project
    Former-commit-id: 3bdea5a55d5677b09c45164f4b1fc011a8bb2b79 [formerly ef388b60516e8977f7c6487247e092de2e8d3c6e]
    Former-commit-id: 6169a286dd2ca428860978a22558a1657ac7bdc6
    Former-commit-id: 017daf86f31745baa521e86cec30161ac5547446 [formerly 1d94158b7b4581c6b9f6fe57f2ea78b076ce3782]
    Former-commit-id: 5e04121975cfc3fbee8933a180ab6695e2054d23
    Former-commit-id: 9020080e1f158711887c4b2dd6e0c638d98180b7 [formerly cec5fb747368dce2a725a53981b8ff96e3bba2e8]
    Former-commit-id: ebec1c32ee0b457bb473126a1e177f111b662164

---
+++ AutomaticImageClassificationWebApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutomaticImageClassificationWebApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomaticImageClassificationWebApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("49c1fda7-7cb5-49ba-92d9-4e544c2d79b0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
